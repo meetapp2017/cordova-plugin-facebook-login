1dd7e5891dfec031055df3c58ffea666a5f27e70
MeetApp <meetapp2017@gmail.com>
2024-11-25 22:37:42 +0200

    init 1

---
+++ www/facebook-login.js
@@ -0,0 +1,11 @@
+var exec = require('cordova/exec');
+
+/**
+ * Метод за логин с Facebook
+ * @param {Object} options - Опции за Facebook логин (например permissions)
+ * @param {Function} success - Callback при успешен логин
+ * @param {Function} error - Callback при грешка
+ */
+exports.loginWithFacebook = function (options, success, error) {
+    exec(success, error, 'facebook-login', 'loginWithFacebook', [options]);
+};
