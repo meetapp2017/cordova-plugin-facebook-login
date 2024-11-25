bee8e3a428cdff133e4906e09d6dca06e94e05e0
MeetApp <meetapp2017@gmail.com>
2024-11-26 00:13:09 +0200

    init 5

--- src/ios/FacebookLogin.m
+++ src/ios/FacebookLogin.m
@@ -1,28 +1,50 @@
 /********* FacebookLogin.m Cordova Plugin Implementation *******/
 
 #import <Cordova/CDV.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
 
-@interface FacebookLogin : CDVPlugin {
-  // Member variables go here.
-}
-
-- (void)loginWithFacebook:(CDVInvokedUrlCommand*)command;
+@interface FacebookLogin : CDVPlugin
 @end
 
 @implementation FacebookLogin
 
-- (void)loginWithFacebook:(CDVInvokedUrlCommand*)command
-{
-    CDVPluginResult* pluginResult = nil;
-    NSString* echo = [command.arguments objectAtIndex:0];
+- (void)loginWithFacebook:(CDVInvokedUrlCommand*)command {
+    NSDictionary* options = [command.arguments objectAtIndex:0];
+    NSString* appId = [options objectForKey:@"appId"];
 
-    if (echo != nil && [echo length] > 0) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
-    } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    if (appId == nil || [appId length] == 0) {
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Facebook App ID is missing"];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        return;
     }
 
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    // Configure Facebook SDK
+    [[FBSDKApplicationDelegate sharedInstance] initializeSDK];
+    [FBSDKSettings.sharedSettings setAppID:appId];
+
+    // Use FBSDKLoginManager for Facebook login
+    FBSDKLoginManager *loginManager = [[FBSDKLoginManager alloc] init];
+    [loginManager logInWithPermissions:@[@"public_profile", @"email"]
+                     fromViewController:self.viewController
+                                handler:^(FBSDKLoginManagerLoginResult *result, NSError *error) {
+        CDVPluginResult* pluginResult = nil;
+
+        if (error) {
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error.localizedDescription];
+        } else if (result.isCancelled) {
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Login cancelled"];
+        } else {
+            NSDictionary *response = @{
+                @"userId": result.token.userID,
+                @"token": result.token.tokenString,
+                @"permissions": result.grantedPermissions.allObjects,
+                @"declinedPermissions": result.declinedPermissions.allObjects
+            };
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:response];
+        }
+
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
 }
 
 @end
